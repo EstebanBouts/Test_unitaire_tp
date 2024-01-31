4bd159be847954191d52d90aaf7efc1cf632a607
EstebanBouts <estebanboutsbonhomme@gmail.com>
2024-01-31 16:05:55 +0100

    Création du test unitaire miroir sans sa méthode à tester

---
+++ test/test_chaine.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_miroir(self):
+        # Test chaîne normale
+        self.assertEqual(miroir("Hello"), "olleH")
+
+        # Test chaîne vide
+        self.assertEqual(miroir(""), "")
+
+        # Test chaîne contenant des espaces
+        self.assertEqual(miroir("Python is fun"), "nuf si nohtyP")
+
+        # Test chaîne contenant des caractères spéciaux
+        self.assertEqual(miroir("!@#$%"), "%$#@!")
+
+
+if __name__ == '__main__':
+    unittest.main()
