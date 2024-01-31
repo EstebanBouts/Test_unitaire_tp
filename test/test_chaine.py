3364fb5325e7115960746dcf009841604acff621
EstebanBouts <estebanboutsbonhomme@gmail.com>
2024-01-31 16:28:02 +0100

    Ajout du test unitaire palindrome sans sa méthode

--- test/test_chaine.py
+++ test/test_chaine.py
@@ -1,5 +1,5 @@
 import unittest
-from miroir import miroir
+from chaine import miroir
 
 
 class MyTestCase(unittest.TestCase):
@@ -16,6 +16,18 @@ class MyTestCase(unittest.TestCase):
 
         # Test chaîne contenant des caractères spéciaux
         self.assertEqual(miroir("!@#$%"), "%$#@!")
+
+    def test_palindrome(self):
+        #Test d'un palindrome
+        self.assertEqual(palindrome("racecar"), "racecar Bien dit")
+
+        # Test d'un non-palindrome
+        self.assertEqual(palindrome("hello"), "hello")
+
+        # Test d'une chaîne vide
+        self.assertEqual(palindrome(""), "")
+
+
 
 
 if __name__ == '__main__':
